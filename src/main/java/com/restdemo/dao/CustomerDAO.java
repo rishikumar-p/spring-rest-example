fc43af0174c9e1cb2bdcbb463341331320dee0fd
rishipebbeti <rishikumarreddy.pebbeti@zemosolabs.com>
2022-03-23 10:25:30 +0530

    Spring REST Example initial commit

---
+++ src/main/java/com/restdemo/dao/CustomerDAO.java
@@ -0,0 +1,17 @@
+package com.restdemo.dao;
+
+import com.restdemo.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h2>Spring REST Demo!</h2>
+<hr>
+<a href="api/customers">Get All Customers</a>
+</body>
+</html>
